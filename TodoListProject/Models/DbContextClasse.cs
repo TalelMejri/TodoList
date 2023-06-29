9e0b62427448458b9e05aea69aa3e6d37dc50119
talel <talelmejri8@gmail.com>
2023-06-29 17:54:52 +0100

    add table todos

---
+++ TodoListProject/Models/DbContextClasse.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace TodoListProject.Models
+{
+    public class DbContextClasse : DbContext
+    {
+        public DbSet<Todos> Todos { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(@"data source=Desktop-d1jfgmm\sqlexpress; initial catalog=TodosDb; integrated security=SSPI;TrustServerCertificate=True");
+        }
+
+    }
+}
--- TodoListProject/Models/Todos.cs
+++ TodoListProject/Models/Todos.cs
@@ -1,7 +1,28 @@
-﻿namespace TodoListProject.Models
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace TodoListProject.Models
 {
+    [Table("Todos")]
     public class Todos
     {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int Id { get; set; }
+
+        [Required]
+        [Column(TypeName = "varchar(50)")]
+        public string? Title { get; set; }
+
+        [Required]
+        [Column(TypeName ="varchar(100)")]
+        public string? Description { get; set; }
+
+        [Required]
+        [DataType(DataType.Date)]
+        public DateTime? DueDate { get; set; }
+
+        public bool IsCompleted { get; set; }
 
     }
 }
